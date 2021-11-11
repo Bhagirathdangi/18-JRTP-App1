63b4ea71f7ce671ced3a60662d5dc564687c18b1
Bhagirath dangi <bhagirathdangi42@gmail.com>
2021-11-11 10:22:13 +0530

    A.java

---
+++ A.java
@@ -0,0 +1,17 @@
+package dangi;
+class A 
+{
+	
+		private void show()
+		{
+		System.out.println("Show");
+		}
+		public static void main(String[] args) 
+	{
+       A a  = new A();
+		   a.show();
+
+        
+
+	}//main
+}//class
